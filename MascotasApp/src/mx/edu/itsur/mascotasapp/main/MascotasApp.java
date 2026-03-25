6583ae723fe335e95cacbd418a4968fd574ed3a2
fjml1983 <fjml1983@gmail.com>
2026-03-25 11:08:35 -0600

    Codigo base del proyecto

---
+++ MascotasApp/src/mx/edu/itsur/mascotasapp/main/MascotasApp.java
@@ -0,0 +1,16 @@
+package mx.edu.itsur.mascotasapp.main;
+
+/**
+ *
+ * @author FJML1983
+ */
+public class MascotasApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
